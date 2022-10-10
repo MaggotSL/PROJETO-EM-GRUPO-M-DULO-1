b61d8edad025e6949d722ef7d7cf51d19af50ee3
Caio de Souza <caio.souza2000@yahoo.com.br>
2022-10-10 17:34:07 -0300

    Projeto em Grupo

--- script.js
+++ script.js
@@ -12,4 +12,17 @@ function nextImage(){
     }
     document.getElementById("radio"+count).checked = true;
 }
-
+// Carrinho 
+("button").on("click", function() {
+    addToBasket(); (this).parent().parent().find(".product_overlay").css({
+      'transform': ' translateY(0px)',
+      'opacity': '1',
+      'transition': 'all ease-in-out .45s'
+    }).delay(1500).queue(function() {
+      (this).css({
+        'transform': 'translateY(-500px)',
+        'opacity': '0',
+        'transition': 'all ease-in-out .45s'
+      }).dequeue();
+    });
+  });
